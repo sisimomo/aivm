7f90d03c07493a381c2be6f75b569e418b18386c
Simon Vallieres <simon@vallieres.ca>
2026-06-21 18:29:20 -0400

    feat: emit Lima portForwards for socket bridges

--- test/unit/vm/lima_template_test.go
+++ test/unit/vm/lima_template_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"os"
 	"os/exec"
+	"strings"
 	"testing"
 	"time"
 
@@ -17,7 +18,7 @@ func TestLimaTemplate_ValidatesWithLimactl(t *testing.T) {
 
 	path, err := vm.LimaTemplatePath([]vm.Mount{
 		{HostPath: "/tmp/aivm-host", GuestPath: "/work", Writable: true},
-	})
+	}, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -30,5 +31,26 @@ func TestLimaTemplate_ValidatesWithLimactl(t *testing.T) {
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		t.Fatalf("limactl template validate: %v\n%s", err, out)
+	}
+}
+
+func TestLimaTemplate_IncludesPortForwards(t *testing.T) {
+	path, err := vm.LimaTemplatePath([]vm.Mount{}, []vm.SocketBridge{{
+		HostPath: "/tmp/host.sock", GuestPath: "/run/aivm/sockets/host.sock",
+	}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(path)
+	data, err := os.ReadFile(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	body := string(data)
+	if !strings.Contains(body, "portForwards:") {
+		t.Fatalf("missing portForwards in template:\n%s", body)
+	}
+	if !strings.Contains(body, "reverse: true") {
+		t.Fatal("missing reverse: true")
 	}
 }
